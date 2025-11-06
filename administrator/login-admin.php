9dfc4855856d3cdc590c683962e2b548381dc434
taira-201 <s.taira0201@icloud.com>
2025-11-06 12:27:36 +0900

    Update login-admin.php

--- administrator/login-admin.php
+++ administrator/login-admin.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>管理者 ログイン</title>
 </head>
 <body>
     <img src="./img/admin-logo.png" alt="Rehone Admin Logo">
     <h1>ログイン</h1>
-    
+
 </body>
 </html>
