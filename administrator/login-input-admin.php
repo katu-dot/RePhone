e99933163f732bfba8050bb390acd1902dba53e2
wapiwapi3 <tomo.geneion@gmail.com>
2025-11-07 11:21:13 +0900

    login修正中

--- administrator/login-input-admin.php
+++ administrator/login-input-admin.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>RePhone_staff ログイン</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
+<?php require './header.php'; ?>
+<?php require '../db-connect.php'; ?>
+<?php session_start(); ?>
+<?php
+// 修正中
+?>
   <div class="login-container">
     <img src="./img/admin-logo.png" alt="">
 
@@ -22,5 +19,4 @@
       <button type="submit">ログイン</button>
     </form>
   </div>
-</body>
-</html>
+<?php require './footer.php'; ?>
